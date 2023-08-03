0e2fc67dafb8fa312d389a7b3784684245e8aa7d
nyakiomwangi <cnyakio@usiu.ac.ke>
2023-08-03 16:15:42 +0300

    added for loop to print out elements in the array

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿// create a simple program that inputs and prints array elements using a loop
+﻿// create a simple program that inputs and prints array elements using a loop ..project1 
 //step 1 ask for user input
+
+using System;
 
 internal class Program
 {
@@ -7,45 +9,23 @@ internal class Program
     {
         Console.WriteLine("Enter the size of the array");
         int size = Convert.ToInt32(Console.ReadLine());
-        //create the array
-        int arr[] = new int[size];
 
-
-        for (int i = 0; i < size; i++)
-        {
-            arr[i] = Convert.ToInt32(Console.ReadLine());
-
-
-        }
-        Console.WriteLine("\n Elements in the array are:");
-    }
-}
-
-// class Program
-{
-    static void Main(string[] args)
-    {
-        //C# program to print all negative elements in array
-        /* Input array size */
-        Console.Write("Enter size of array: ");
-        int size = Convert.ToInt32(Console.ReadLine());
+        // Create the array
         int[] arr = new int[size];
-        /* Input elements in array */
         Console.WriteLine("Enter {0} elements in the array", size);
         for (int i = 0; i < size; i++)
         {
             arr[i] = Convert.ToInt32(Console.ReadLine());
         }
-        /*Print all negative elements of array*/
-        Console.Write("\nAll negative elements in array are : ");
+
+        Console.WriteLine("\n Elements in the array are:");
+
         for (int i = 0; i < size; i++)
         {
-            /* If current array element is negative */
-            if (arr[i] < 0)
-            {
-                Console.Write("{0}\t", arr[i]);
-            }
+            Console.Write("{0} ", arr[i]);
         }
+
         Console.ReadLine();
     }
 }
+
